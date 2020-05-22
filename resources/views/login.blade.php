cae9cd244c5eb86cd4a089a7bc57dd70a412822d
MisterClass <misterclass2909@yandex.ru>
2020-05-22 13:12:11 +0300

    Add login header style

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello World</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    </head>
+    <body>
+      <div id="loginBG">
+        <header>
+          <div class="limitation">
+            <a href="#" id = "header-logo">
+              <h3>Hello World!</h3>
+            </a>
+          </div>
+        </header>
+
+        <main>
+
+        </main>
+      </div>
+    </body>
+</html>
