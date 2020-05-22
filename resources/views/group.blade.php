4792838b9213807ca716f937f5c2a8fbc07f86e4
MisterClass <misterclass2909@yandex.ru>
2020-05-23 00:40:03 +0300

    Add prototype student tasks list in group

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -25,6 +25,33 @@
           <div class="clipboards"></div>
         </div>
       </div>
+      <div id="student-tasks">
+        <div class="student-info">
+          <div class="user-avatar"></div>
+          <p>Кто-то Тамович</p>
+        </div>
+        <div class="tasks-list">
+          <div class="student-task">
+            <div class="task-lang"></div>
+            <div class="task-content">
+              <p>Калькулятор</p>
+            </div>
+            <div class="task-status task-done">
+              <p>Решена</p>
+            </div>
+          </div>
+
+          <div class="student-task">
+            <div class="task-lang"></div>
+            <div class="task-content">
+              <p>Калькулятор</p>
+            </div>
+            <div class="task-status task-unresolved">
+              <p>Нерешена</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
