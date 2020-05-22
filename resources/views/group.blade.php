a4b4138807bc37816b6c8111b17aee4464377167
MisterClass <misterclass2909@yandex.ru>
2020-05-23 00:07:04 +0300

    Add prototype students list in group

---
+++ resources/views/group.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.layout')
+
+@section('content')
+  <div class="limitation">
+    <div id="students-group" class = "flex-between">
+      <div id="students-list">
+        <div class="student-card">
+          <div class="user-avatar"></div>
+          <p>Кто-то Тамович</p>
+          <div class="clipboards"></div>
+        </div>
+        <div class="student-card">
+          <div class="user-avatar"></div>
+          <p>Кто-то Тамович</p>
+          <div class="clipboards"></div>
+        </div>
+        <div class="student-card">
+          <div class="user-avatar"></div>
+          <p>Кто-то Тамович</p>
+          <div class="clipboards"></div>
+        </div>
+        <div class="student-card">
+          <div class="user-avatar"></div>
+          <p>Кто-то Тамович</p>
+          <div class="clipboards"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
