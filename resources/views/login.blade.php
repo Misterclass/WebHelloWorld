7655c5bd6929cc7541d185f431a8325a96f59605
MisterClass <misterclass2909@yandex.ru>
2020-05-22 14:25:23 +0300

    Add login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,7 +19,19 @@
         </header>
 
         <main>
-
+          <div class="limitation flex-row-end">
+            <form class="container-form" id = "login-form" action="" method="post">
+              <h2>Мы рады, что ты вернулся!</h2>
+              <p>Новые задачи уже ждут тебя!</p>
+              <div class="form-inputs">
+                <input type="text" name="email" placeholder = "Электронная почта или логин">
+                <input type="password" name="password" placeholder = "Пароль">
+              </div>
+              <div class="form-buttons">
+                <input type="submit" value="Авторизоваться" class = "btn-pink">
+              </div>
+            </form>
+          </div>
         </main>
       </div>
     </body>
