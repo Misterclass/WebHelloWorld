35a5f6ab2ba1790ad7bcf6a9f7e79509b0099ba5
MisterClass <misterclass2909@yandex.ru>
2020-05-24 18:48:03 +0300

    Add mobile adaptivity home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,24 @@
           <h3>Решена</h3>
         </div>
       </div>
+      <div class="task-mobile">
+        <div class="flex">
+          <div class="task-lang"></div>
+          <div class="task-content">
+            <h3>Калькулятор</h3>
+            <p>Автор: Кто-то Тамович</p>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <div class="task-status task-done">
+          <h3>Решена</h3>
+        </div>
+      </div>
       <div class="task">
         <div class="task-lang"></div>
         <div class="task-content">
@@ -31,6 +49,24 @@
             nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
+        <div class="task-status task-unresolved">
+          <h3>Нерешена</h3>
+        </div>
+      </div>
+      <div class="task-mobile">
+        <div class="flex">
+          <div class="task-lang"></div>
+          <div class="task-content">
+            <h3>Калькулятор</h3>
+            <p>Автор: Кто-то Тамович</p>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat.
+        </p>
         <div class="task-status task-unresolved">
           <h3>Нерешена</h3>
         </div>
