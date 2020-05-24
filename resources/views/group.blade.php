631570d3d1bd482ad8e33da99d457d58eedd618a
MisterClass <misterclass2909@yandex.ru>
2020-05-24 19:27:15 +0300

    Add mobile adaptivity students page

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -5,24 +5,64 @@
     <div id="students-group" class = "flex-between">
       <div id="students-list">
         <div class="student-card">
-          <div class="user-avatar"></div>
-          <p>Кто-то Тамович</p>
-          <div class="clipboards"></div>
+          <div class="student-card-header">
+            <div class="user-avatar"></div>
+            <p>Кто-то Тамович</p>
+            <div class="clipboards"></div>
+          </div>
+          <div class="student-card-tasks">
+            <div class="tasks-list">
+              <div class="student-task">
+                <div class="task-lang"></div>
+                <div class="task-content">
+                  <p>Калькулятор</p>
+                </div>
+                <div class="task-status task-done">
+                  <p>Решена</p>
+                </div>
+              </div>
+
+              <div class="student-task">
+                <div class="task-lang"></div>
+                <div class="task-content">
+                  <p>Калькулятор</p>
+                </div>
+                <div class="task-status task-unresolved">
+                  <p>Нерешена</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="student-card">
-          <div class="user-avatar"></div>
-          <p>Кто-то Тамович</p>
-          <div class="clipboards"></div>
-        </div>
-        <div class="student-card">
-          <div class="user-avatar"></div>
-          <p>Кто-то Тамович</p>
-          <div class="clipboards"></div>
-        </div>
-        <div class="student-card">
-          <div class="user-avatar"></div>
-          <p>Кто-то Тамович</p>
-          <div class="clipboards"></div>
+          <div class="student-card-header">
+            <div class="user-avatar"></div>
+            <p>Кто-то Тамович</p>
+            <div class="clipboards"></div>
+          </div>
+          <div class="student-card-tasks">
+            <div class="tasks-list">
+              <div class="student-task">
+                <div class="task-lang"></div>
+                <div class="task-content">
+                  <p>Калькулятор</p>
+                </div>
+                <div class="task-status task-done">
+                  <p>Решена</p>
+                </div>
+              </div>
+
+              <div class="student-task">
+                <div class="task-lang"></div>
+                <div class="task-content">
+                  <p>Калькулятор</p>
+                </div>
+                <div class="task-status task-unresolved">
+                  <p>Нерешена</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div id="student-tasks">
