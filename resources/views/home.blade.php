9ea77b8cff164e6b905468414b09bc1d2c3c88e0
MisterClass <misterclass2909@yandex.ru>
2020-06-19 19:21:12 +0300

    Add home snippets mobile version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,6 +48,54 @@
       return link;
     }
 
+    function GetHtmlSnippetMobile(snippet)
+    {
+      let link = document.createElement('a');
+      link.setAttribute("href", "http://127.0.0.1:8000/snippets/" + snippet.id);
+
+      let task = document.createElement('div');
+      task.className = "task-mobile";
+
+      let taskStatusClass = "task-" + snippet.linenos;
+      let taskStatus = document.createElement('div');
+      taskStatus.className = "task-status";
+      taskStatus.classList.add(taskStatusClass);
+
+      let taskDesc = document.createElement('p');
+      taskDesc.textContent = snippet.description;
+
+      //Create content
+      let taskContent = document.createElement('div');
+      taskContent.className = "task-content";
+
+      let taskHeader = document.createElement('h3');
+      taskHeader.textContent = snippet.title;
+
+      let taskAuthor = document.createElement('p');
+      let user = JSON.parse(sessionStorage.getItem('token'));
+      taskAuthor.textContent = "Автор: " + user.username;
+
+      taskContent.appendChild(taskHeader);
+      taskContent.appendChild(taskAuthor);
+
+      //Create avatar
+      let taskLang = document.createElement('div');
+      taskLang.className = "task-lang";
+
+      let flex = document.createElement('div');
+      flex.className = "flex";
+
+      flex.appendChild(taskLang);
+      flex.appendChild(taskContent);
+
+      task.appendChild(flex);
+      task.appendChild(taskDesc);
+      task.appendChild(taskStatus);
+      link.appendChild(task);
+
+      return link;
+    }
+
     $(document).ready(function()
     {
       $.ajax({
@@ -66,8 +114,9 @@
           snippets.forEach((item, i) =>
           {
             let html = GetHtmlSnippet(item);
-            // let htmlMobile = GetHtmlSnippetMobile(item);
+            let htmlMobile = GetHtmlSnippetMobile(item);
             taskList.appendChild(html);
+            taskList.appendChild(htmlMobile);
           });
 
         },
@@ -77,74 +126,6 @@
 
   <div class="limitation">
     <div class="tasks-list">
-        <!-- <div class="task">
-          <div class="task-lang"></div>
-          <div class="task-content">
-            <h3>Калькулятор</h3>
-            <p>Автор: Кто-то Тамович</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-          <div class="task-status task-done">
-
-          </div>
-        </div> -->
-        <!-- <div class="task-mobile">
-          <div class="flex">
-            <div class="task-lang"></div>
-            <div class="task-content">
-              <h3>Калькулятор</h3>
-              <p>Автор: Кто-то Тамович</p>
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <div class="task-status task-done">
-
-          </div>
-        </div>
-        <div class="task">
-          <div class="task-lang"></div>
-          <div class="task-content">
-            <h3>Калькулятор</h3>
-            <p>Автор: Кто-то Тамович</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-          <div class="task-status task-unresolved">
-
-          </div>
-        </div>
-        <div class="task-mobile">
-          <div class="flex">
-            <div class="task-lang"></div>
-            <div class="task-content">
-              <h3>Калькулятор</h3>
-              <p>Автор: Кто-то Тамович</p>
-            </div>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-          <div class="task-status task-unresolved">
-
-          </div>
-        </div> -->
     </div>
     <div class="flex-center">
       <a href="{{ route('snippets.create') }}">
