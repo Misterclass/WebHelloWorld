9b404a9c699e2bb479767d73771cbba9fc1fe396
MisterClass <misterclass2909@yandex.ru>
2020-06-19 18:29:23 +0300

    Add student page

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -10,89 +10,55 @@
       </div>
       <div class="user-avatar"></div>
       <div id="student-content">
-        <h3>Кто-то Тамович</h3>
+        <h3>{{ $currentUser->username }}</h3>
         <h3>Звание: <i>Студент</i></h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          {{ $currentUser->description }}
         </p>
       </div>
     </div>
     <div class="tasks-list">
       <h2 class = "text-center">Список задач</h2>
+      @foreach($currentUser->snippets as $snippet)
       <div class="task">
         <div class="task-lang"></div>
         <div class="task-content">
-          <h3>Калькулятор</h3>
-          <p>Автор: Кто-то Тамович</p>
+          <h3>{{ $snippet->title }}</h3>
+          <p>Автор: {{ $currentUser->username }}</p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
+            {{ $snippet->description }}
           </p>
         </div>
-        <div class="task-status task-done">
-          
-        </div>
-      </div>
-      <div class="task">
-        <div class="task-lang"></div>
-        <div class="task-content">
-          <h3>Калькулятор</h3>
-          <p>Автор: Кто-то Тамович</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat.
-          </p>
-        </div>
+        @if ($snippet->linenos == "done")
+          <div class="task-status task-done">
+
+          </div>
+        @else
         <div class="task-status task-unresolved">
 
         </div>
+        @endif
       </div>
       <div class="task-mobile">
         <div class="flex">
           <div class="task-lang"></div>
           <div class="task-content">
-            <h3>Калькулятор</h3>
-            <p>Автор: Кто-то Тамович</p>
+            <h3>{{ $snippet->title }}</h3>
+            <p>Автор: {{ $currentUser->username }}</p>
           </div>
         </div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
+          {{ $snippet->description }}
         </p>
-        <div class="task-status task-done">
+        @if ($snippet->linenos == "done")
+          <div class="task-status task-done">
 
-        </div>
-      </div>
-<div class="task-mobile">
-        <div class="flex">
-          <div class="task-lang"></div>
-          <div class="task-content">
-            <h3>Калькулятор</h3>
-            <p>Автор: Кто-то Тамович</p>
           </div>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </p>
+        @else
         <div class="task-status task-unresolved">
 
         </div>
+        @endif
       </div>
-    </div>
-  </div>
+      @endforeach
 @endsection
